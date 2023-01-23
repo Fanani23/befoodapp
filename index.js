9374859acdbe6da6c652611d5850db335adf5e39
Fanani23 <pramudiasf23@gmail.com>
2023-01-23 17:40:13 +0700

    back to befood

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://befoodapp.vercel.app/",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
